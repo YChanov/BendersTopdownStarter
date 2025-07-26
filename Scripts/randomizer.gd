f616b413ebcb332298c5bbe34343c486782b6b52
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2025-07-26 23:18:41 +0300

    Randomizer test

--- Scripts/randomizer.gd
+++ Scripts/randomizer.gd
@@ -1,46 +1,57 @@
 extends Node
-# Configuration for tile placement
-# Use your tile IDs here (from your tileset)
-# These IDs are the atlas coordinates or names defined in the tileset
+
 @export var tile_config := {
-	"a": Vector2i(1, 1),
-	"s": Vector2i(6, 3),
-	"q": Vector2i(7, 3),
-	"d": Vector2i(8, 3),
-	"e": Vector2i(6, 4),
-	"f": Vector2i(7, 4),
-	"g": Vector2i(8, 4),
-	"h": Vector2i(6, 5),
-	"j": Vector2i(7, 5),
-	"k": Vector2i(8, 5),
-	"z": Vector2i(6, 4),
-	"x": Vector2i(6, 4),
-	"v": Vector2i(6, 4),
-	"b": Vector2i(6, 4),
+	"1": { "coords": Vector2i(6, 3), "weight": 50 },
+	"2": { "coords": Vector2i(7, 3), "weight": 10 },
+	"3": { "coords": Vector2i(8, 3), "weight": 50 },
+	"4": { "coords": Vector2i(6, 4), "weight": 50 },
+	"5": { "coords": Vector2i(7, 4), "weight": 10 },
+	"6": { "coords": Vector2i(8, 4), "weight": 30 },
+	"7": { "coords": Vector2i(6, 5), "weight": 50 },
+	"8": { "coords": Vector2i(7, 5), "weight": 10 },
+	"9": { "coords": Vector2i(8, 5), "weight": 50 },
 }
 
+@export var tile_area_size := Vector2i(100, 100)
 @onready var tilemap: TileMapLayer = $"../Scene/TileMap"
 
-@export var tile_area_size := Vector2i(100, 100) # Area size around the player
+var weighted_tile_pool: Array = []
 
 func _ready():
-	generate_random_environment()
+	if tilemap:
+		_build_weighted_tile_pool()
+		generate_random_environment()
+	else:
+		push_error("TileMapLayer not found!")
+	var new_scene = PackedScene.new()
+	new_scene.pack(tilemap) 
+	ResourceSaver.save(new_scene, "res://generated_level.tscn")
 	
+
+
+func _build_weighted_tile_pool():
+	weighted_tile_pool.clear()
+	for tile_name in tile_config:
+		var tile_info = tile_config[tile_name]
+		var weight = tile_info.get("weight", 1)
+		var coords = tile_info.get("coords")
+		for i in range(weight):
+			weighted_tile_pool.append(coords)
+
+func _get_random_tile_coords() -> Vector2i:
+	if weighted_tile_pool.is_empty():
+		return Vector2i.ZERO
+	return weighted_tile_pool[randi() % weighted_tile_pool.size()]
 
 func generate_random_environment():
 	tilemap.clear()
 
-	var tile_keys := tile_config.keys()
 	var area_half := tile_area_size / 2
 
 	for x in range(-area_half.x, area_half.x):
 		for y in range(-area_half.y, area_half.y):
-			var world_pos = Vector2i(x, y)
-
-			# Pick a random tile type
-			var random_key = tile_keys[randi() % tile_keys.size()]
-			var tile_atlas_coord = tile_config[random_key]
-
-			tilemap.set_cell(world_pos, 1, tile_atlas_coord)
+			var pos = Vector2i(x, y)
+			var atlas_coords = _get_random_tile_coords()
+			tilemap.set_cell(pos, 1, atlas_coords)
 
 	tilemap.update_internals()
