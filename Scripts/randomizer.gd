0ceb798ce5fea18bef4875469919a42a933bd1de
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2025-07-27 00:08:26 +0300

    Randomizer test

--- Scripts/randomizer.gd
+++ Scripts/randomizer.gd
@@ -1,40 +1,48 @@
 extends Node
 
-@export var tile_config := {
-	"1": { "coords": Vector2i(6, 3), "weight": 50 },
-	"2": { "coords": Vector2i(7, 3), "weight": 10 },
-	"3": { "coords": Vector2i(8, 3), "weight": 50 },
-	"4": { "coords": Vector2i(6, 4), "weight": 50 },
-	"5": { "coords": Vector2i(7, 4), "weight": 10 },
-	"6": { "coords": Vector2i(8, 4), "weight": 30 },
-	"7": { "coords": Vector2i(6, 5), "weight": 50 },
-	"8": { "coords": Vector2i(7, 5), "weight": 10 },
-	"9": { "coords": Vector2i(8, 5), "weight": 50 },
-}
+@export var tile_config: Array[Dictionary] = [
+	{ "coords": Vector2i(6, 3), "weight": 10 },
+	{ "coords": Vector2i(7, 3), "weight": 1 },
+	{ "coords": Vector2i(8, 3), "weight": 10 },
+	{ "coords": Vector2i(6, 4), "weight": 10 },
+	{ "coords": Vector2i(7, 4), "weight": 1 },
+	{ "coords": Vector2i(8, 4), "weight": 1 },
+	{ "coords": Vector2i(6, 5), "weight": 10 },
+	{ "coords": Vector2i(7, 5), "weight": 1 },
+	{ "coords": Vector2i(8, 5), "weight": 10 },
+	{ "coords": Vector2i(15, 3), "weight": 10 },
+	{ "coords": Vector2i(16, 3), "weight": 10 },
+	{ "coords": Vector2i(17, 3), "weight": 10 },
+	{ "coords": Vector2i(15, 4), "weight": 10 },
+	{ "coords": Vector2i(16, 4), "weight": 10 },
+	{ "coords": Vector2i(17, 4), "weight": 10 },
+	{ "coords": Vector2i(15, 5), "weight": 10 },
+	{ "coords": Vector2i(16, 5), "weight": 10 },
+	{ "coords": Vector2i(17, 5), "weight": 10 }
+]
+
+const TILE_SET_SOURCE_ID = 1
 
 @export var tile_area_size := Vector2i(100, 100)
 @onready var tilemap: TileMapLayer = $"../Scene/TileMap"
 
-var weighted_tile_pool: Array = []
+var weighted_tile_pool: Array[Vector2i] = []
 
 func _ready():
 	if tilemap:
 		_build_weighted_tile_pool()
 		generate_random_environment()
+		var new_scene := PackedScene.new()
+		new_scene.pack(tilemap)
+		ResourceSaver.save(new_scene, "res://generated_level.tscn")
 	else:
 		push_error("TileMapLayer not found!")
-	var new_scene = PackedScene.new()
-	new_scene.pack(tilemap) 
-	ResourceSaver.save(new_scene, "res://generated_level.tscn")
-	
-
 
 func _build_weighted_tile_pool():
 	weighted_tile_pool.clear()
-	for tile_name in tile_config:
-		var tile_info = tile_config[tile_name]
+	for tile_info in tile_config:
 		var weight = tile_info.get("weight", 1)
-		var coords = tile_info.get("coords")
+		var coords = tile_info.get("coords", Vector2i.ZERO)
 		for i in range(weight):
 			weighted_tile_pool.append(coords)
 
@@ -50,8 +58,8 @@ func generate_random_environment():
 
 	for x in range(-area_half.x, area_half.x):
 		for y in range(-area_half.y, area_half.y):
-			var pos = Vector2i(x, y)
-			var atlas_coords = _get_random_tile_coords()
-			tilemap.set_cell(pos, 1, atlas_coords)
+			var pos := Vector2i(x, y)
+			var atlas_coords := _get_random_tile_coords()
+			tilemap.set_cell(pos, TILE_SET_SOURCE_ID, atlas_coords)
 
 	tilemap.update_internals()
