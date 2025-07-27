c66791daa5bf5514142ed257077039da67311a02
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2025-07-27 20:12:10 +0300

    Slime roam around

--- Scenes/NPC's/Enemy/Scripts/States/EnemyIdleState.gd
+++ Scenes/NPC's/Enemy/Scripts/States/EnemyIdleState.gd
@@ -1,8 +1,18 @@
 extends State
 class_name enemy_idle_state
 
+@onready var body = $"../.."
+@export var move_speed := float(30)
+
+var chase_direction
 func Enter():
+	chase_direction = Vector2(randi_range(-100,100), randi_range(-100,100)) - body.position as Vector2
+	while true:
+		chase_direction = chase_direction.rotated(45)
+		await get_tree().create_timer(3).timeout
 	pass
 
 func Update(_delta):
-	pass
+	body.velocity = chase_direction.normalized() * move_speed
+	body.move_and_slide()
+	await get_tree().create_timer(2).timeout
